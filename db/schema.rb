c2c6c0bb90ea90e80685ccda868a2d2ca42297a4
Marcelo <marcelo.cansilva@gmail.com>
2021-04-16 21:23:44 -0300

    add schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_14_004546) do
+ActiveRecord::Schema.define(version: 2021_04_17_001354) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_04_14_004546) do
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "images"
   end
 
   create_table "reviews", force: :cascade do |t|
